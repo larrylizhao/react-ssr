bdf3199eb9172b80b04a549de0225430d9daf3df
larrylizhao <larrylizhao@gmail.com>
2022-06-06 23:19:34 +0800

    Different ways of render components on server

--- src/server/server.js
+++ src/server/server.js
@@ -3,10 +3,12 @@ const express = require('express');
 const app = express();
 const React = require('react');
 const ReactDOMServer = require('react-dom/server');
-const App = require('../client/es5/app-commonjs');
+const {default: App} = require('./app');
+const {default: appCommon} = require('../client/es5/app');
+const AppCommon = require('../client/es5/app-commonjs');
 
-//renderToString 接收的是React Element, server端无法使用未经编译的jsx<App />
-const html = ReactDOMServer.renderToString(React.createElement(App, null));
+//renderToString 接收的是React Element, server端无法使用未经编译的jsx<App />, 需要先经babel转译或者webpack打包(libraryTarget: "commonjs2")
+const html = ReactDOMServer.renderToString(React.createElement(AppCommon, null));
 const port = process.env.PORT || 5000;
 
 app.use('/server', (req, res) => {
--- webpack.config.server.js
+++ webpack.config.server.js
@@ -1,6 +1,7 @@
 const path= require('path');
 
-//用于build ./src/client/app.js 以供 ./server/server.js 使用
+// 用于build ./src/client/app.js 以供 ./server/server.js 使用 ❌
+// webpack打包的目的是为了给前端使用，server.js并无法使用打包后的component. 如果要给server.js使用直接用babel转译
 module.exports = {
     target: 'node',
     mode: 'development',
@@ -8,7 +9,8 @@ module.exports = {
     entry: path.join(__dirname, 'src/client/app'),
     output: {
         filename: "app.js",
-        path: path.resolve(__dirname, 'src/server')
+        path: path.resolve(__dirname, 'src/server'),
+        libraryTarget: "commonjs2"
     },
     module: {
         rules: [
